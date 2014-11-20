60e932e1e9f73fb7f64f87ea99635cb7d53cd25b
Gregory Loden <gregory.loden@gmail.com>
2014-11-20 14:54:56 -0500

    simple tile dragging test

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//	void OnMouseDown() {
+//	}
+//	void OnMouseUp() {
+//	}
+	void OnMouseDrag() {
+		Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		gameObject.transform.position = cursorPosition;
+	}
+}
