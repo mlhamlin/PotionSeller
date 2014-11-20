7df45b6b11a6824799e60c5b869ad663c026dd1d
Kevin Cavanaugh <kevrcav@gmail.com>
2014-11-20 16:26:42 -0500

    Adding ingredient and ingredient generator

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ingredient : MonoBehaviour {
+
+    public Color color;
+    public int intl, str, cha, dex, cost, level;
+    string[] AtrbIncrease = { "intl", "str", "cha", "dex" };
+
+    // 1 intl, 2 str, 3 cha, 4 dex
+    public void IncreaseIntl(int amt)
+    {
+        intl += amt;
+    }
+
+    public void IncreaseIntl(int amt)
+    {
+        str += amt;
+    }
+
+    public void IncreaseIntl(int amt)
+    {
+        dex += amt;
+    }
+
+    public void IncreaseIntl(int amt)
+    {
+        cha += amt;
+    }
+}
---
+++ Assets/Scripts/IngredientGenerator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientGenerator : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+    Ingredient GenerateIngredient()
+    {
+        return new Ingredient();
+    }
+}
