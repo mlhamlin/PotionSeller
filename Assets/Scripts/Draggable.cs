f9958107314601249a4be4fd966da10fc07a1c21
Gregory Loden <gregory.loden@gmail.com>
2014-11-24 14:59:48 -0500

    finish up drag and drop script

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -5,16 +5,34 @@ using System.Collections.Generic;
 public class Draggable : MonoBehaviour {
 	List<Collider2D> colliders = new List<Collider2D>();
 
+	public void OnTriggerEnter2D(Collider2D other) {
+		colliders.Add(other);
+//		Debug.Log("Entering: " + colliders.Count + " colliders");
+	}
+	public void OnTriggerExit2D(Collider2D other) {
+		colliders.Remove(other);
+//		Debug.Log("Exiting: " + colliders.Count + " colliders");
+	}
+
 	public void OnMouseDrag() {
 		Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		gameObject.transform.position = cursorPosition;
 	}
-	public void OnTriggerEnter2D(Collider2D other) {
-		colliders.Add(other);
-		Debug.Log("Entering: " + colliders.Count + " colliders");
-	}
-	public void OnTriggerExit2D(Collider2D other) {
-		colliders.Remove(other);
-		Debug.Log("Exiting: " + colliders.Count + " colliders");
+	public void OnMouseUp() {
+		if (colliders.Count > 0) {
+			Vector2 pos = gameObject.transform.position;
+			Vector2 colliderPos = colliders[0].transform.position;
+			//manhattan distance
+			float dist = Mathf.Abs(pos.x - colliderPos.x) + Mathf.Abs(pos.y - colliderPos.y);
+			for (int i = 1; i < colliders.Count; i += 1) {
+				Vector2 colliderPos2 = colliders[i].transform.position;
+				float dist2 = Mathf.Abs(pos.x - colliderPos2.x) + Mathf.Abs(pos.y - colliderPos2.y);
+				if (dist2 < dist) {
+					dist = dist2;
+					colliderPos = colliderPos2;
+				}
+			}
+			gameObject.transform.position = colliderPos;
+		}
 	}
 }
